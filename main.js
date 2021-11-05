5b9255b16bbfbb98b412e0631aa7d704c84e9a09
cspdevs000 <cspdevs000@gmail.com>
2021-11-05 02:15:51 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let newImage = document.querySelectorAll('img');
+newImage[1].src = "./0_1straswitch_465_466_int.jpeg";
+
+let newBand = document.querySelector('.title');
+newBand.textContent = 'Strawberry Switchblade';
+
+let newParagraph = document.querySelectorAll('p');
+newParagraph[0].textContent = 'Strawberry Switchblade were a Scottish new wave/pop duo formed in Glasgow in 1981 by Jill Bryson and Rose McDowall, best known for their song "Since Yesterday" from 1985, and their flamboyant clothing with bows and polka-dots.'
+newParagraph[1].textContent = 'Jill Bryson';
+newParagraph[2].textContent = '@thetopcatjill';
+
+let newDiv = document.querySelectorAll('.content');
+newDiv[0].textContent = 'Had to say goodbye to my beautiful 19 year old cat Pigu today. So so sad.';
+newDiv[1].textContent = 'Check out the film of my Glasgow performance in full below.';
+newDiv[2].textContent = 'Alan Horne interviews Viv Albertine of the Slits for the never published "Strawberry Switchblade" 1979';
+
+newImage[2].src = "./7GkjlZFm_400x400.jpeg";
+newImage[3].src = "./7GkjlZFm_400x400.jpeg";
+newImage[4].src = "./7GkjlZFm_400x400.jpeg";
